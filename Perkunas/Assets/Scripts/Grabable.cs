7163aa6a2be6931c375a3833af304efdf2f9bfad
Simon Ragnesand <binamrad@gmail.com>
2017-09-13 09:30:02 +0200

    First iteration of interaction scripts
    Added scripts for grabbing and dropping stuff. Should be easy to extend.

---
+++ Perkunas/Assets/Scripts/Grabable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabable : MonoBehaviour {
+	public GameObject attachedTo = null;
+	// Use this for initialization
+	void Start () {
+		if (attachedTo != null) {
+			this.gameObject.transform.parent = attachedTo.transform;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void attach(GameObject g) {
+		attachedTo = g;
+		this.gameObject.transform.parent = g.transform;
+	}
+
+	public void detach() {
+		attachedTo = null;
+		this.gameObject.transform.parent = null;
+	}
+}
---
+++ Perkunas/Assets/Scripts/Grabber.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabber : MonoBehaviour {
+	//information of what we will grab next
+	LinkedList<Collision> colliders = new LinkedList<Collision>();
+	GameObject grabbed = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	//these two functions track what objects are currently colliding with the grabber
+	void OnCollisionEnter (Collision col)
+	{
+		colliders.AddFirst (col);
+	}
+
+	void OnCollisionExit (Collision col) {
+		colliders.Remove (col);
+	}
+
+	public void grab() {
+		if (colliders.Count > 0) {
+			Grabable g = colliders.First.Value.gameObject.GetComponent<Grabable> ();
+				if(g != null) {
+					grabbed = colliders.First.Value.gameObject;
+					g.attach(this.gameObject);
+				}
+		}
+	}
+
+	public void ungrab() {
+		grabbed.GetComponent<Grabable>().detach ();
+		grabbed = null;
+	}
+}
