08f2915962385bca22672f3593ca216698a68cf1
magnyf <francois.magny@laposte.net>
2017-09-14 22:15:08 +0200

    Fire simulation
    Add fire on a true as a particule system
    + script to activate the fire with the remote of the vive, TO CHANGE
    with the way we want to start the fire

---
+++ Perkunas/Assets/Scripts/Flame.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flame : MonoBehaviour {
+
+
+	private SteamVR_TrackedObject trackedObj;
+
+
+	void Awake() {
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+
+
+	private SteamVR_Controller.Device Controller {
+		get { return SteamVR_Controller.Input((int)trackedObj.index); }
+	}
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<ParticleSystem> ().Clear();
+		GetComponent<ParticleSystem> ().Pause ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (false) {
+			if (Controller.GetPress (SteamVR_Controller.ButtonMask.Grip)) {
+				GetComponent<ParticleSystem> ().Play ();
+			} 	
+		}
+	}
+}
