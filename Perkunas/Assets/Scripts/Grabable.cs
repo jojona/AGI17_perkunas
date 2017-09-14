dc18e0a72bbebb28cc5ea4a067ec3bd3f32582f4
Simon Ragnesand <binamrad@gmail.com>
2017-09-14 21:54:16 +0200

    Added terrain modification.
    Grab the terrain, move it around, and it changes shape.

--- Perkunas/Assets/Scripts/Grabable.cs
+++ Perkunas/Assets/Scripts/Grabable.cs
@@ -3,21 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Grabable : MonoBehaviour {
-	public GameObject attachedTo = null;
+	private GameObject attachedTo = null;
 		
 
 	// Use this for initialization
 	void Start () {
-		if (attachedTo != null) {
-			this.gameObject.transform.parent = attachedTo.transform;
-		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
 	}
-	public void attach(GameObject g) {
+	public virtual void attach(GameObject g) {
 		FixedJoint fx = g.gameObject.AddComponent<FixedJoint>();
     		fx.breakForce = 20000;
     		fx.breakTorque = 20000;
@@ -26,7 +23,7 @@ public class Grabable : MonoBehaviour {
 		attachedTo = g;
 	}
 
-	public void detach(Vector3 vel, Vector3 ang) {
+	public virtual void detach(Vector3 vel, Vector3 ang) {
 		if (attachedTo != null && attachedTo.GetComponent<FixedJoint>())
 			{
 			// 2
---
+++ Perkunas/Assets/Scripts/GrabableTerrain.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabableTerrain : Grabable {
+	GameObject manipulator = null;
+
+	public float effectRadius = 1.0f;
+	private float resolutionPerUnit;
+	public Terrain terrain;
+	// Use this for initialization
+	void Start () {
+		if (terrain == null) {
+			terrain = GetComponent<Terrain> ();
+		}
+		if (terrain == null) {
+			Debug.Log ("no terrain in GrabableTerrain");
+			return;
+		}
+		resolutionPerUnit = ((float)terrain.terrainData.heightmapResolution) / 10.0f;//((float)terrain.terrainData.heightmapWidth);
+	}
+
+
+	private float max(float a, float b) {
+		return a > b ? a : b;
+	}
+	private float min(float a, float b) {
+		return a < b ? a : b;
+	}
+	// Update is called once per frame
+	void Update () {
+		if (manipulator != null && terrain != null) {
+			Vector2 manipRelPos = new Vector2 (manipulator.transform.position.x - transform.position.x, manipulator.transform.position.z- transform.position.z);
+			float manipRelHeight = manipulator.transform.position.y - transform.position.y;
+			int absX = (int)(manipRelPos.x * resolutionPerUnit);
+			int absY = (int)(manipRelPos.y * resolutionPerUnit);
+			float relHeightAbs = manipRelHeight - terrain.terrainData.GetHeight (absX, absY);
+			float newHeight = manipRelHeight / 600;
+			newHeight = newHeight > 1 ? 1 : newHeight;
+			newHeight = newHeight < 0 ? 0 : newHeight;
+			int cubeWidth = (int)(effectRadius * resolutionPerUnit);
+			Debug.Log ("relative position: "+ manipRelPos + ", relative height: " + manipRelHeight + ", relative height abs: " + relHeightAbs);
+			Debug.Log ("absX: "+ absX + ", absY: " + absY);
+			Debug.Log ("resu: "+ resolutionPerUnit);
+			int maxDist = cubeWidth/2;
+			float offset = (float) 0.1 / 600;
+			if(newHeight > terrain.terrainData.GetHeight(absX - maxDist, absY - maxDist) + offset) {
+				float[,] data = terrain.terrainData.GetHeights (absX - maxDist, absY - maxDist, cubeWidth, cubeWidth);
+				Debug.Log("terrain height: " + data[0,0]);
+				for (int x = 0; x < cubeWidth; ++x) {
+					for (int y = 0; y < cubeWidth; ++y) {
+						float dist = (x - (maxDist))*(x-(maxDist));
+						dist += (y - (maxDist)) * (y - (maxDist));
+						dist = Mathf.Sqrt (dist);
+						data [x, y] = max(newHeight * (float)Mathf.Cos ((float)((dist / maxDist) * 0.5 * Mathf.PI)), data [x, y]);
+						data [x, y] = data [x, y] < 0.0f ? 0.0f : data [x, y];
+					}
+				}
+				Debug.Log ("set to: " + data [0, 0]);
+
+				terrain.terrainData.SetHeights(absX - maxDist, absY - maxDist, data);
+			} else if(newHeight < terrain.terrainData.GetHeight(absX - maxDist, absY - maxDist) - offset){
+				float[,] data = terrain.terrainData.GetHeights (absX - maxDist, absY - maxDist, cubeWidth, cubeWidth);
+				Debug.Log("terrain height: " + data[0,0]);
+				for (int x = 0; x < cubeWidth; ++x) {
+					for (int y = 0; y < cubeWidth; ++y) {
+						float dist = (x - (maxDist))*(x-(maxDist));
+						dist += (y - (maxDist)) * (y - (maxDist));
+						dist = Mathf.Sqrt (dist);
+						data [x, y] = min((1.0f-newHeight) * (float)Mathf.Cos ((float)((dist / maxDist) * 0.5 * Mathf.PI)), data [x, y]);
+						data [x, y] = data [x, y] > 1.0f ? 0.0f : data [x, y];
+					}
+				}
+				Debug.Log ("set to: " + data [0, 0]);
+
+				terrain.terrainData.SetHeights(absX - maxDist, absY - maxDist, data);
+			}
+		}
+	}
+
+	public override void attach(GameObject g) {
+		manipulator = g;
+	}
+
+	public override void detach(Vector3 a, Vector3 b) {
+		manipulator = null;
+		//we do nothing special with the release speeds here
+	}
+}
