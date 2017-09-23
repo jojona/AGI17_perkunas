364d7c0428e0cc1d5dfff2384cc3b2488431edcb
Antoine Ladrech <antoine.ladrech@hotmail.fr>
2017-09-23 19:36:36 +0200

    Shake

---
+++ Perkunas/Assets/Scripts/ShakeCloud.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeCloud : MonoBehaviour {
+
+    private bool isRaining = false;
+    private bool isShaken = false;
+
+    public Rigidbody rb;
+    public ParticleSystem rain;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rain = GetComponentInChildren<ParticleSystem>();
+        rain.Stop();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (rb.velocity.magnitude >= 10 && !isShaken)
+        {
+            isShaken = true;
+            isRaining = !isRaining;
+            if (isRaining)
+                rain.Play();
+            else
+                rain.Stop();
+        }
+        if (rb.velocity.magnitude < 10)
+            isShaken = false;
+
+    }
+}
