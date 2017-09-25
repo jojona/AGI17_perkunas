5089aba4a114f4e65ebeb2c9d2cf784217b3d009
Zinzano <svalan@gmail.com>
2017-09-25 19:26:43 +0200

    Create RandomRotation script
    - a script that change the rotation of the gameobject to a radom choosen rotation around the Y-axis, i.e. the up axis, usually the Z-axis

---
+++ Perkunas/Assets/RandomRotation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotation : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+		float angle = Random.Range (0, 360);
+		transform.Rotate (0, angle, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+}
