b9b53ebc18183d0c6f52ee9ce6e94b18d1bd3c4a
Antoine Ladrech <antoine.ladrech@hotmail.fr>
2017-09-26 20:41:30 +0200

    Shaking Cloud tested without Vive + Spawn Trees

--- Perkunas/Assets/Scripts/ShakeCloud.cs
+++ Perkunas/Assets/Scripts/ShakeCloud.cs
@@ -7,20 +7,34 @@ public class ShakeCloud : MonoBehaviour {
     private bool isRaining = false;
     private bool isShaken = false;
 
-    public Rigidbody rb;
-    public ParticleSystem rain;
-    
+    private Rigidbody rb;
+    private ParticleSystem rain;
+    private float timer;
+    private float nextActionTime = 2;
+    private float period = 1;
+    private GameObject[] trees;
+
     void Start()
     {
+        timer = -1; //so we can shake right away
         rb = GetComponent<Rigidbody>();
         rain = GetComponentInChildren<ParticleSystem>();
         rain.Stop();
     }
-	
-	// Update is called once per frame
-	void Update () {
-		if (rb.velocity.magnitude >= 10 && !isShaken)
+
+    void GrowATree()
+    {
+        Vector3 vec = new Vector3(rb.position.x, 1, rb.position.z);
+        Quaternion quat = new Quaternion();
+
+        Instantiate(GameObject.Find("Tree"), vec, quat);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (rb.velocity.magnitude >= 2 && !isShaken && (Time.time - timer >= 1))
         {
+            timer = Time.time;
             isShaken = true;
             isRaining = !isRaining;
             if (isRaining)
@@ -28,8 +42,14 @@ public class ShakeCloud : MonoBehaviour {
             else
                 rain.Stop();
         }
-        if (rb.velocity.magnitude < 10)
+        if (rb.velocity.magnitude < 2 && (Time.time - timer >= 1))
             isShaken = false;
 
+        if (Time.time > nextActionTime && isRaining)
+        {
+            trees = GameObject.FindGameObjectsWithTag("TreePrefab");
+            nextActionTime += period;
+            GrowATree();
+        }
     }
 }
---
+++ Perkunas/Assets/Scripts/Testing/CloudMoves.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMoves : MonoBehaviour {
+
+    public Rigidbody rb;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(0, 0, -5);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Time.time >= 5)
+            rb.velocity = new Vector3(0, 0, 0);
+        if (Time.time >= 7)
+            rb.velocity = new Vector3(5, 0, 0);
+    }
+}
