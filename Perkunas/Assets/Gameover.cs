9d8a673dfc7c0f1e11befd4ff8c98c063af9d072
Johan Jonasson <jojona@kth.se>
2017-09-27 20:30:07 +0200

    Time limit

---
+++ Perkunas/Assets/Gameover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameover : MonoBehaviour {
+
+	public GameObject canvas;
+
+	float startTime;
+
+	// Use this for initialization
+	void Start () {
+		canvas.SetActive (false);
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.time - startTime > 120) {
+			canvas.SetActive (true);
+		}
+
+
+	}
+}
