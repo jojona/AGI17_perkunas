bf0f61639c0ba39105ee6075af68644904e62ba9
Antoine Ladrech <antoine.ladrech@hotmail.fr>
2017-10-12 15:50:26 +0200

    Random wandreing + stopping

--- Perkunas/Assets/Scripts/AnimalBehavior.cs
+++ Perkunas/Assets/Scripts/AnimalBehavior.cs
@@ -4,13 +4,55 @@ using UnityEngine;
 
 public class AnimalBehavior : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float speed = 0.05f;
+    public float directionChangeInterval = 1;
+    public float maxHeadingChange = 30;
+    
+    float heading;
+    Vector3 targetRotation;
+    Rigidbody rb;
+    CharacterController cc;
+    float sleepTime;
+    bool asleep = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cc = GetComponent<CharacterController>();
+
+        // Set random initial rotation
+        heading = Random.Range(0, 360);
+        transform.eulerAngles = new Vector3(0, heading, 90);
+    }
+
+    void Update()
+    {
+        if (asleep)
+        {
+            sleepTime -= Time.deltaTime;
+            if (sleepTime < 0)
+                asleep = false;
+        }
+
+        else
+        {
+            float odd = Mathf.Min(0.10f / (1.0f / (Time.deltaTime)), 1);
+            float rand = Random.Range(0.0f, 1.0f);
+
+            Debug.Log(odd);
+            Debug.Log(rand);
+            if (rand < odd)
+            {
+                asleep = true;
+                sleepTime = Random.Range(2f, 7f);
+            }
+            var floor = Mathf.Clamp(heading - maxHeadingChange, 0, 360);
+            var ceil = Mathf.Clamp(heading + maxHeadingChange, 0, 360);
+            heading = Random.Range(floor, ceil);
+            targetRotation = new Vector3(0, heading, 90);
+            transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, targetRotation, Time.deltaTime * directionChangeInterval);
+            var forward = transform.TransformDirection(new Vector3(0, 1, 0));
+            cc.SimpleMove(speed * forward);
+        }
+    }
 }
